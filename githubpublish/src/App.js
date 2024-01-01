fbdcc526cb315a7a415667b9619bdf393ad315d6
Prasanna Venkatesh M <m.prasannavenkatesh9@outlook.com>
2024-01-01 14:11:01 +0530

    Initial Commit

---
+++ githubpublish/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h4>Demo App to Publish</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
